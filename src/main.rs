5d705d4b505879d331fee6c8681f44f5e1fc3212
Ali Raheem <ali@sc1425.home>
2015-10-12 20:38:08 +0100

    Added constant for default length

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ extern crate rand;
 use rand::{thread_rng, Rng};
 use std::env;
 
+const DEFAULT_LEN: usize = 8;
+
 fn main() {
     let len: usize = match env::args().nth(1) {
-        Some(s) => { match s.parse() { Ok(s) => s, Err(_) => 8} },
-	None => { 8 },
+        Some(s) => { match s.parse() { Ok(s) => s, Err(_) => DEFAULT_LEN} },
+	None => { DEFAULT_LEN },
     };
     let pass: String = thread_rng().gen_ascii_chars().take(len).collect();
     println!("{}", pass);
